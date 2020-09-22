fde0936c3e60da016caf60113fff8f8434af29b5
keudaz <44206750+keudaz@users.noreply.github.com>
2020-09-22 22:37:19 +0530

    db

---
+++ db/s_company.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 22, 2020 at 07:06 PM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `s_company`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `disease`
+--
+
+CREATE TABLE `disease` (
+  `id` int(11) NOT NULL,
+  `r_id` int(11) NOT NULL,
+  `plant` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `scientific` varchar(250) NOT NULL,
+  `scentiment` varchar(250) NOT NULL,
+  `image_1` varchar(1000) NOT NULL,
+  `image_2` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `disease`
+--
+
+INSERT INTO `disease` (`id`, `r_id`, `plant`, `scientific`, `scentiment`, `image_1`, `image_2`) VALUES
+(1, 1, 'කතුරුමුරුංගා', 'sesbaniya grandiflora', '8.6', '1.jpg', '3.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recommendation`
+--
+
+CREATE TABLE `recommendation` (
+  `id` int(11) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `description` varchar(250) NOT NULL,
+  `image` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `recommendation`
+--
+
+INSERT INTO `recommendation` (`id`, `name`, `description`, `image`) VALUES
+(1, 'Man', 'zzzzzzzzz', '4.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `disease`
+--
+ALTER TABLE `disease`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `recommendation`
+--
+ALTER TABLE `recommendation`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `disease`
+--
+ALTER TABLE `disease`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `recommendation`
+--
+ALTER TABLE `recommendation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
