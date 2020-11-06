1542ad760bde0a60f8c999c22468b3c3cc26e95b
keudaz <44206750+keudaz@users.noreply.github.com>
2020-11-06 18:29:23 +0530

    db

--- db/s_company.sql
+++ db/s_company.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 22, 2020 at 07:06 PM
+-- Generation Time: Nov 06, 2020 at 01:58 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
@@ -25,6 +25,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL,
+  `comment` varchar(250) CHARACTER SET utf16 COLLATE utf16_bin NOT NULL,
+  `user` varchar(250) NOT NULL,
+  `date` date NOT NULL,
+  `type` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`id`, `comment`, `user`, `date`, `type`) VALUES
+(3, 'bvngc', 'k@gmail.com', '2020-11-06', 2),
+(4, 'ew', 'k@gmail.com', '2020-11-06', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `disease`
 --
 
@@ -32,18 +54,23 @@ CREATE TABLE `disease` (
   `id` int(11) NOT NULL,
   `r_id` int(11) NOT NULL,
   `plant` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `plant_1` varchar(250) CHARACTER SET utf16 COLLATE utf16_bin NOT NULL,
   `scientific` varchar(250) NOT NULL,
+  `scientific_1` varchar(250) NOT NULL,
   `scentiment` varchar(250) NOT NULL,
+  `scentiment_1` varchar(250) NOT NULL,
   `image_1` varchar(1000) NOT NULL,
-  `image_2` varchar(1000) NOT NULL
+  `image_2` varchar(1000) NOT NULL,
+  `pro_1` int(11) NOT NULL,
+  `pro_2` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `disease`
 --
 
-INSERT INTO `disease` (`id`, `r_id`, `plant`, `scientific`, `scentiment`, `image_1`, `image_2`) VALUES
-(1, 1, 'කතුරුමුරුංගා', 'sesbaniya grandiflora', '8.6', '1.jpg', '3.jpg');
+INSERT INTO `disease` (`id`, `r_id`, `plant`, `plant_1`, `scientific`, `scientific_1`, `scentiment`, `scentiment_1`, `image_1`, `image_2`, `pro_1`, `pro_2`) VALUES
+(1, 1, 'කතුරුමුරුංගා', 'කතුරුම', 'sesbaniya grandiflora', 'grandiflora', '8.6', '52.0', '1.jpg', '3.jpg', 35, 85);
 
 -- --------------------------------------------------------
 
@@ -65,9 +92,37 @@ CREATE TABLE `recommendation` (
 INSERT INTO `recommendation` (`id`, `name`, `description`, `image`) VALUES
 (1, 'Man', 'zzzzzzzzz', '4.jpg');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(250) NOT NULL,
+  `lname` varchar(250) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fname`, `lname`, `email`, `password`, `date`) VALUES
+(2, 'k', 'k', 'k@gmail.com', '8ce4b16b22b58894aa86c421e8759df3', '2020-11-06');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `disease`
@@ -82,8 +137,20 @@ ALTER TABLE `recommendation`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `disease`
@@ -96,6 +163,12 @@ ALTER TABLE `disease`
 --
 ALTER TABLE `recommendation`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
